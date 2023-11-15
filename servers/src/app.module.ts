55816ba8db09c74048f14807e2586300190cf3b7
tuhl22052306 <tuhl22052306@q1.com>
2023-11-15 16:25:57 +0800

    feat: 服务端添加配置文件

--- servers/src/app.module.ts
+++ servers/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ServeStaticModule, ServeStaticModuleOptions } from '@nestjs/serve-static'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import * as path from 'path'
+
+import configuration from './config/index'
 
 @Module({
-    imports: [],
+    imports: [
+        // 配置模块
+        ConfigModule.forRoot({
+            cache: true,
+            load: [configuration],
+            isGlobal: true,
+        }),
+        // 服务静态化, 生产环境最好使用 nginx 做资源映射， 可以根据环境配置做区分
+        ServeStaticModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                const fileUploadLocationConfig =
+                    config.get<string>('app.file.location') || '../upload'
+                const rootPath = path.isAbsolute(fileUploadLocationConfig)
+                    ? `${fileUploadLocationConfig}`
+                    : path.join(process.cwd(), `${fileUploadLocationConfig}`)
+                return [
+                    {
+                        rootPath,
+                        exclude: [`${config.get('app.prefix')}`],
+                        serveRoot: config.get('app.file.serveRoot'),
+                        serveStaticOptions: {
+                            cacheControl: true,
+                        },
+                    },
+                ] as ServeStaticModuleOptions[]
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- servers/src/main.ts
+++ servers/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+    const app = await NestFactory.create(AppModule, {
+        cors: true,
+    })
+
+    const config = app.get(ConfigService)
+
+    // 设置 api 访问前缀
+    const prefix = config.get<string>('app.prefix')
+    app.setGlobalPrefix(prefix)
+
+    await app.listen(8080)
 }
 bootstrap()
