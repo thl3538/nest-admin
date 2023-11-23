9aa9dec0c13f8ef6abec77b610e1bb1430199745
tuhl22052306 <tuhl22052306@q1.com>
2023-11-23 16:10:22 +0800

    feat: 服务端增加日志

--- servers/src/main.ts
+++ servers/src/main.ts
@@ -2,6 +2,17 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 
+import express from 'express'
+import path from 'path'
+
+import { logger } from './common/libs/log4js/logger.middleware'
+import { Logger } from './common/libs/log4js/log4j.util'
+import { TransformInterceptor } from './common/libs/log4js/transform.interceptor'
+import { HttpExceptionsFilter } from './common/libs/log4js/http-exceptions-filter'
+import { ExceptionsFilter } from './common/libs/log4js/exceptions-filter'
+
+import Chalk from 'chalk'
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: true,
@@ -13,6 +24,34 @@ async function bootstrap() {
     const prefix = config.get<string>('app.prefix')
     app.setGlobalPrefix(prefix)
 
-    await app.listen(8080)
+    // 日志
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.use(logger)
+    // 使用全局拦截器打印出参
+    app.useGlobalInterceptors(new TransformInterceptor())
+    // 所有异常
+    app.useGlobalFilters(new ExceptionsFilter())
+    app.useGlobalFilters(new HttpExceptionsFilter())
+    // 获取配置端口
+    const port = config.get<number>('app.port') || 8080
+    await app.listen(port)
+
+    const fileUploadLocationConfig = config.get<string>('app.file.location') || '../upload'
+    const fileUploadBastPath = path.normalize(
+        path.isAbsolute(fileUploadLocationConfig)
+            ? `${fileUploadLocationConfig}`
+            : path.join(process.cwd(), `${fileUploadLocationConfig}`),
+    )
+    Logger.log(
+        Chalk.green(`Nest-Admin 服务启动成功 `),
+        '\n',
+        Chalk.green('上传文件存储路径'),
+        `        ${fileUploadBastPath}`,
+        '\n',
+        Chalk.green('服务地址'),
+        `                http://localhost:${port}${prefix}/`,
+        '\n',
+    )
 }
 bootstrap()
